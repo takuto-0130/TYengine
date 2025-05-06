1d141eaf90e49e3c7fc92ac9f650b271505800b0
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-05-06 17:47:42 +0900

    temp

--- CopyPass.cpp
+++ CopyPass.cpp
@@ -29,20 +29,6 @@ void CopyPass::Initialize(DirectXBasis* dxBasis, SrvManager* srvMgr, const std::
     auto ps = dxBasis_->CompileShader(psPath, L"ps_6_0");
 
     D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc{};
-    /*psoDesc.VS = { vs->GetBufferPointer(), vs->GetBufferSize() };
-    psoDesc.PS = { ps->GetBufferPointer(), ps->GetBufferSize() };
-    psoDesc.pRootSignature = rootSignature_.Get();
-    psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-    psoDesc.NumRenderTargets = 1;
-    psoDesc.SampleDesc.Count = 1;
-    psoDesc.SampleMask = UINT_MAX;
-    psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-    psoDesc.InputLayout = { nullptr, 0 };
-    psoDesc.DepthStencilState.DepthEnable = FALSE;
-    psoDesc.DepthStencilState.StencilEnable = FALSE;
-    psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
-    psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);*/
-
     psoDesc.VS = { vs->GetBufferPointer(), vs->GetBufferSize() };
     psoDesc.PS = { ps->GetBufferPointer(), ps->GetBufferSize() };
     psoDesc.pRootSignature = rootSignature_.Get();
@@ -69,12 +55,12 @@ void CopyPass::Draw(ID3D12GraphicsCommandList* cmdList, D3D12_GPU_DESCRIPTOR_HAN
     cmdList->SetGraphicsRootSignature(rootSignature_.Get());
     cmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-    // ✅ SRVヒープとSamplerヒープを両方指定
+    // SRVヒープとSamplerヒープを両方指定
     ID3D12DescriptorHeap* heaps[] = {
-        srvMgr_->GetHeap(),   // CBV/SRV/UAV用
-        dxBasis_->GetSamplerHeap()              // Sampler用
+        srvMgr_->GetHeap(),             // CBV/SRV/UAV用
+        dxBasis_->GetSamplerHeap()      // Sampler用
     };
-    cmdList->SetDescriptorHeaps(_countof(heaps), heaps); // ✅ 2つを一括バインド
+    cmdList->SetDescriptorHeaps(_countof(heaps), heaps); // 2つを一括バインド
 
     cmdList->SetGraphicsRootDescriptorTable(0, srvHandle);
     cmdList->SetGraphicsRootDescriptorTable(1, dxBasis_->GetSamplerDescriptorHandle());

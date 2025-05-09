05bd0689f42c991dc4f2370fbe785db76b5d5dc7
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-05-09 11:45:32 +0900

    Smoothing5x5

--- Resources/Shaders/Smoothing.PS.hlsl
+++ Resources/Shaders/Smoothing.PS.hlsl
@@ -9,18 +9,22 @@ struct PixelShaderOutput
     float4 color : SV_TARGET0;
 };
 
-static const float2 kIndex3x3[3][3] =
+static const float2 kIndex5x5[5][5] =
 {
-    { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
-    { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
-    { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
+    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
+    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } },
 };
 
-static const float kKernel3x3[3][3] =
+static const float kKernel5x5[5][5] =
 {
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25 },
+    { 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25 },
+    { 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25 },
+    { 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25 },
+    { 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25, 1.0f / 25 },
 };
 
 PixelShaderOutput main(VertexShaderOutput input)
@@ -29,27 +33,25 @@ PixelShaderOutput main(VertexShaderOutput input)
     uint width, height;
     gTexture.GetDimensions(width, height);
     float2 uvStepSize = float2(rcp(float(width)), rcp(float(height)));
-    
-    
+
     output.color.rgb = float3(0.0f, 0.0f, 0.0f);
     output.color.a = 1.0f;
-    for (int x = 0; x < 3; ++x)
-    {
-        for (int y = 0; y < 3; ++y)
-        {
-            float2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
-            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-            output.color.rgb += fetchColor * kKernel3x3[x][y];
 
+    for (int y = 0; y < 5; ++y)
+    {
+        for (int x = 0; x < 5; ++x)
+        {
+            float2 texcoord = input.texcoord + kIndex5x5[y][x] * uvStepSize;
+            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+            output.color.rgb += fetchColor * kKernel5x5[y][x];
         }
     }
-    
-    
+
     if (input.texcoord.x < 0.0f || input.texcoord.x > 1.0f ||
-    input.texcoord.y < 0.0f || input.texcoord.y > 1.0f)
+        input.texcoord.y < 0.0f || input.texcoord.y > 1.0f)
     {
         discard;
     }
-    
+
     return output;
 }

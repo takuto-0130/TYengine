1498bdab18c2f7bd52bee4296ec3abb3394b6233
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-05-30 13:00:30 +0900

    トリガーでエフェクトが出せるように

--- engine/Framework/GameCore.cpp
+++ engine/Framework/GameCore.cpp
@@ -47,12 +47,12 @@ void GameCore::Initialize()
 	auto ring = std::make_unique<RingParticle>();
 	auto cylinder = std::make_unique<CylinderParticle>();
 
-	/*int index = particleManager->Add(std::move(plane));
-	int indexRing = particleManager->Add(std::move(ring));*/
+	int index = particleManager->Add(std::move(plane));
+	int indexRing = particleManager->Add(std::move(ring));
 	particleManager->Add(std::move(cylinder));
 
 	particleManager->InitializeAll(directXBasis, srvManager.get(), camera.get());
-	/*IParticleRenderer::Emitter emitter;
+	IParticleRenderer::Emitter emitter;
 	emitter.transform.scale = { 0.05f,1.0f,1.0f };
 	emitter.transform.rotate = { 0,0,0 };
 	emitter.transform.translate = { 0,0,0 };
@@ -64,9 +64,9 @@ void GameCore::Initialize()
 	emitterRing.transform.scale = { 0.5f,0.5f,0.5f };
 	emitterRing.transform.rotate = { 0,0,0 };
 	emitterRing.transform.translate = { 0,0,0 };
-	emitterRing.count = 1;
+	emitterRing.count = 4;
 	emitterRing.frequency = 1.5f;
-	particleManager->SetEmitter(indexRing, emitterRing);*/
+	particleManager->SetEmitter(indexRing, emitterRing);
 }
 
 void GameCore::Finalize()

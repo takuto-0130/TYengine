44baac7436faa7fede6e203115d673d6a5415b77
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-06-11 16:30:32 +0900

    リソースフォルダ整理

--- application/Score/score.cpp
+++ application/Score/score.cpp
@@ -5,37 +5,38 @@
 
 void score::Initialze()
 {
-	TextureManager::GetInstance()->LoadTexture("Resources/number.png");
+	TextureManager::GetInstance()->LoadTexture("Resources/Texture/number.png");
+	TextureManager::GetInstance()->LoadTexture("Resources/Texture/white2x2.png");
 	one_ = std::make_unique<Sprite>();
-	one_->Initialize("Resources/number.png");
+	one_->Initialize("Resources/Texture/number.png");
 	one_->SetTextureSize({ 64,64 });
 	one_->SetTextureLeftTop({ 128,0 });
 	one_->SetPosition({ 1000,550 });
 	one_->SetSize({ 64,64 });
 
 	two_ = std::make_unique<Sprite>();
-	two_->Initialize("Resources/number.png");
+	two_->Initialize("Resources/Texture/number.png");
 	two_->SetTextureSize({ 64,64 });
 	two_->SetTextureLeftTop({ 64,0 });
 	two_->SetPosition({ 45 + 1000,550 });
 	two_->SetSize({ 64,64 });
 
 	three_ = std::make_unique<Sprite>();
-	three_->Initialize("Resources/number.png");
+	three_->Initialize("Resources/Texture/number.png");
 	three_->SetTextureSize({ 64,64 });
 	three_->SetTextureLeftTop({ 192,0 });
 	three_->SetPosition({ 90 + 1000,550 });
 	three_->SetSize({ 64,64 });
 
 	four_ = std::make_unique<Sprite>();
-	four_->Initialize("Resources/number.png");
+	four_->Initialize("Resources/Texture/number.png");
 	four_->SetTextureSize({ 64,64 });
 	four_->SetTextureLeftTop({ 0,0 });
 	four_->SetPosition({ 135 + 1000,550 });
 	four_->SetSize({ 64,64 });
 
 	back_ = std::make_unique<Sprite>();
-	back_->Initialize("Resources/white2x2.png");
+	back_->Initialize("Resources/Texture/white2x2.png");
 	back_->SetSize({ 215,80 });
 	back_->SetPosition({ 990,542 });
 	back_->SetColor({ 100.f / 256.f,160.f / 256.f,7.f / 256.f,1.0f });

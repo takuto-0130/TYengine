fc7716339248f3ff3eeb6f2714f1b83158170a72
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-06-11 13:27:48 +0900

    フォルダ整理

---
+++ application/Stage/Stage.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Sprite.h"
+#include "Object3d.h"
+#include "WorldTransform.h"
+#include <sstream>
+#include "Audio/Audio.h"
+#include "Rail/Rail.h"
+#include "Object/Enemy/EnemyManager.h"
+#include "Rail/RailEditor.h"
+#include "Skydome/Skydome.h"
+#include "ParticleManager.h"
+#include <memory>
+#include <vector>
+#include <list>
+#include <unordered_set>
+
+class Stage
+{
+public:
+
+};
+

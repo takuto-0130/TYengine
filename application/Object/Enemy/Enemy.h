d6bc14f010de98d939095e1701ff35a0268064d2
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-06-20 10:45:57 +0900

    ベースキャラクター作成

--- application/Object/Enemy/Enemy.h
+++ application/Object/Enemy/Enemy.h
@@ -1,9 +1,11 @@
 #pragma once
-#include "BaseObject.h"
+#include "../BaseCharacter/BaseCharacter.h"
 #include "IParticleRenderer.h"
 
+class IParticleRenderer;
+
 class Enemy :
-    public BaseObject
+    public BaseCharacter
 {
 public:
 	~Enemy() override;
@@ -14,15 +16,7 @@ public:
 
 	void Draw() override;
 
-	Vector3 GetWorldPosition() const;
-
-	bool IsDead() const { return isDead_; }
-
-	void SetRotate(const Vector3& rota) { worldTransform_.rotation_ = rota; }
-
-	void IsCollision() { isDead_ = true; }
-
-	void SetPos(Vector3 pos) { 
+	void SetAndApplyPos(Vector3 pos) { 
 		worldTransform_.translation_ = pos;
 		worldTransform_.TransferMatrix();
 	}

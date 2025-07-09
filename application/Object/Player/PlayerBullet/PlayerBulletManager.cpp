ddeca86434989dd3f1efde555d0a46e08ff1b81c
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-07-09 13:49:02 +0900

    プレイヤーの弾管理クラスの作成開始

--- application/Object/Player/PlayerBullet/PlayerBulletManager.cpp
+++ application/Object/Player/PlayerBullet/PlayerBulletManager.cpp
@@ -1,1 +1,24 @@
 #include "PlayerBulletManager.h"
+#include "../../BaseBullet/BaseBullet.h"
+
+
+void PlayerBulletManager::Init(Player* player)
+{
+	player_ = player;
+}
+
+void PlayerBulletManager::Update()
+{
+	for (auto&& bullet : bullets_)
+	{
+		bullet->Update();
+	}
+}
+
+void PlayerBulletManager::Draw()
+{
+	for (auto&& bullet : bullets_)
+	{
+		bullet->Draw();
+	}
+}
--- application/Object/Player/PlayerBullet/PlayerBulletManager.h
+++ application/Object/Player/PlayerBullet/PlayerBulletManager.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <list>
+#include <memory>
+
+class Player;
+class BaseBullet;
 class PlayerBulletManager
 {
+public:
+	void Init(Player* player);
+	void Update();
+	void Draw();
+
+
+private:
+	Player* player_ = nullptr;
+	std::list<std::unique_ptr<BaseBullet>> bullets_;
 };
 

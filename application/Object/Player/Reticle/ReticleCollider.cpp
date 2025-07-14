5e4da707351d4f8b31842d1c86e26b65ee0b9140
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-07-14 12:30:07 +0900

    判定タイプの追加等

---
+++ application/Object/Player/Reticle/ReticleCollider.cpp
@@ -0,0 +1,13 @@
+#include "ReticleCollider.h"
+
+void ReticleCollider::OnCollisionEnter([[maybe_unused]] Collider& other, [[maybe_unused]] const CollisionInfo& info)
+{
+}
+
+void ReticleCollider::OnCollisionStay([[maybe_unused]] Collider& other, [[maybe_unused]] const CollisionInfo& info)
+{
+}
+
+void ReticleCollider::OnCollisionExit([[maybe_unused]] Collider& other, [[maybe_unused]] const CollisionInfo& info)
+{
+}
---
+++ engine/Utility/Collider/Ray/RayCollider.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Collider.h"
+#include "struct.h"
+class RayCollider :
+    public Collider
+{
+public:
+    RayCollider(uint32_t typeID, const Vector3& origin, Vector3 dir)
+        : Collider(typeID), ray_(Ray{ origin,dir })
+    {
+    }
+
+    ColliderShape GetShapeType() const override { return ColliderShape::RAY; }
+
+    std::optional<Vector3> GetCenter() const override { return ray_.origine; }
+
+    Vector3 GetDiff() const { return ray_.diff; }
+
+    Ray GetRay() const { return ray_; }
+
+    void Update(const Vector3& pos) override { ray_.origine = pos; }
+
+    void SetDirection(const Vector3& dir) { ray_.diff = dir; }
+
+private:
+    Ray ray_;
+};
+

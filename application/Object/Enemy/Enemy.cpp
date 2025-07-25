cb6b47c192b3f9429cf41333022ed2a4976a72e5
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-07-25 09:42:16 +0900

    temp

--- application/Object/Enemy/Enemy.cpp
+++ application/Object/Enemy/Enemy.cpp
@@ -14,7 +14,6 @@ void Enemy::Init()
 	obj_ = std::make_unique<Object3d>();
 	obj_->Initialize();
 	obj_->SetModel("unitSphere.obj");
-	obj_->SetColor({ 1.0f,0.0f,0.0f,1.0f });
 	obj_->SetEnvironmentCoefficient(1.0f);
 	worldTransform_.Initialize();
 	worldTransform_.scale_ = defaultScale_;

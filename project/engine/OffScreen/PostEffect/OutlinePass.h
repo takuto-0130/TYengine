83269d0b65a653d7bef0009bda36265b38d198fc
takuto-0130 <k023g0005@g.neec.ac.jp>
2025-07-27 18:09:49 +0900

    setter/getter追加

--- project/engine/OffScreen/PostEffect/OutlinePass.h
+++ project/engine/OffScreen/PostEffect/OutlinePass.h
@@ -11,6 +11,19 @@ public:
 
 	void ImGuiUpdate();
 
+public:
+	// Setter
+	void SetThreshold(float t) { outlineParam_->depthThreshold = t; }
+	void SetOutlineWidth(float w) { outlineParam_->outlineWidth = w; }
+	void SetTexelSize(const Vector2& texel) { outlineParam_->texelSize = texel; }
+	void SetOutlineColor(const Vector3& color) { outlineParam_->outlineColor = color; }
+
+	// Getter
+	float GetThreshold() const { return outlineParam_->depthThreshold; }
+	float GetOutlineWidth() const { return outlineParam_->outlineWidth; }
+	Vector2 GetTexelSize() const { return outlineParam_->texelSize; }
+	Vector3 GetOutlineColor() const { return outlineParam_->outlineColor; }
+
 private:
 	struct OutlinePassParam {
 		Vector2 texelSize = { 1.0f / 1280.0f, 1.0f / 720.0f }; // レンダリングターゲットの解像度
--- project/engine/OffScreen/PostEffect/RandomEffect.h
+++ project/engine/OffScreen/PostEffect/RandomEffect.h
@@ -11,6 +11,17 @@ class RandomEffect :
 
     void Apply(RenderTexture* input) override;
 
+public:
+    // Setter
+    void SetT(float t) { param_->t = t; }
+    void SetNoiseStrength(float s) { param_->noiseStrength = s; }
+    void SetNoiseTint(const Vector3& c) { param_->noiseTint = c; }
+
+    // Getter
+    float GetT() const { return param_->t; }
+    float GetNoiseStrength() const { return param_->noiseStrength; }
+    Vector3 GetNoiseTint() const { return param_->noiseTint; }
+
 private:
     struct RandomParam {
         float t = 1.0f;

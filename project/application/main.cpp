4951e3f449ef5cc0cebee1f75e20400e927249eb
takuto-0130 <k023g0005@g.neec.ac.jp>
2026-02-05 02:42:51 +0900

    namespace

--- project/application/main.cpp
+++ project/application/main.cpp
@@ -3,7 +3,7 @@
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) 
 {
 	// ゲーム本体のインスタンス作成
-	std::unique_ptr<TYFrameWork> game = std::make_unique<GameCore>();
+	std::unique_ptr<TYEngine::Framework::TYFrameWork> game = std::make_unique<GameCore>();
 
 	// メインループ実行
 	game->run();
